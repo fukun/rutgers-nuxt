aa4dce67f6c8f74fd9b25311ec8934147aefef00
fukun <fukun175@qq.com>
2025-07-31 00:20:32 +0800

    111

--- app/pages/content.vue
+++ app/pages/content.vue
@@ -2,22 +2,13 @@
 //原文件名[...slug]
 const route = useRoute()
 
-const { data: page } = await useAsyncData('page-' + route.path, () => {
-  return queryCollection('content').path(route.path).first()
-})
+// 使用 useContentQuery 替代 queryCollection
 
-if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
-}
 </script>
 
 <template>
   <div>
-    <div >xxx</div>
-    <ContentRenderer
-      v-if="page"
-      :value="page"
-    />
+    <div>xxx</div>
   </div>
 
 </template>
