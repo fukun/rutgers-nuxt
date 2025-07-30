1fc379cca156095687ced510710cde51c4800802
fukun <fukun175@qq.com>
2025-07-31 00:03:49 +0800

    111

---
+++ app/components/faq.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="py-[20px] border-t">
+        <div class="flex justify-between items-center cursor-pointer" @click="toggle()">
+            <span class="text-[17px]">{{ title }}</span>
+
+            <MinusIcon class="w-[20px] h-[20px]" v-if="expanded"></MinusIcon>
+            <PlusIcon class="w-[20px] h-[20px]" v-else></PlusIcon>
+        </div>
+        <div v-if="expanded" class="mt-[20px]">
+            {{ content }}
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { MinusIcon, PlusIcon } from '@heroicons/vue/24/solid';
+
+const props = defineProps<{ title: string; content: string }>();
+const expanded = ref(false);
+
+function toggle() {
+    expanded.value = !expanded.value;
+}
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -169,6 +169,22 @@
                 <a-white-bg title="Request More Info"></a-white-bg>
             </div>
         </div>
+        <div class="ink-black-bg text-white">
+            <div class="container">
+                <div class="w-[780px] mx-auto border-b">
+                    <div class="flex justify-between items-center pt-[40px] pb-[30px]">
+                        <span class="text-[24px]">Top FAQs</span>
+                        <a class="text-[17px] hover:underline" href="">Top FAQs</a>
+                    </div>
+                    <div>
+                        <faq v-for="i of 8" :key="i" :title="`${i}、How are HBS Online courses delivered`"
+                            content="Our platform features short, highly produced videos of HBS faculty and guest business experts, interactive graphs and exercises, cold calls to keep you engaged, and opportunities to contribute to a vibrant online community.">
+                        </faq>
+                    </div>
+                </div>
+                <div class="h-[70px]"></div>
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
