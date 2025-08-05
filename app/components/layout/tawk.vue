00b6135335145bffc904b3c2c23fdb20bde73083
fukun <fukun175@qq.com>
2025-08-05 14:07:13 +0800

    111

--- app/components/layout/tawk.vue
+++ app/components/layout/tawk.vue
@@ -22,7 +22,7 @@ window.Tawk_LoadStart = new Date();
 onMounted(() => {
     const s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/689157e65f8e2b192420f257/1j1rruo8t';
+    s1.src = 'https://embed.tawk.to/68917689205309192bd91530/1j1s3e2kp';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0?.parentNode?.insertBefore(s1, s0);
